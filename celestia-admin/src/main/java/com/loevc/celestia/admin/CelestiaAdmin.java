bb1d0ba3494dd48bd10fdb2a729c1d4b199f3e85
loevc <33386034+loevc@users.noreply.github.com>
2023-11-17 15:42:34 +0800

    feat(feature-v0.0.3): a failed test 🦌

---
+++ celestia-admin/src/main/java/com/loevc/celestia/admin/CelestiaAdmin.java
@@ -0,0 +1,11 @@
+package com.loevc.celestia.admin;
+
+/**
+ * @Author Loevc
+ * @Email Loevc1024@gmail.com
+ * @Date 2023/11/17 15:38
+ * @Description TODO
+ * @Version 1.0
+ */
+public class CelestiaAdmin {
+}
